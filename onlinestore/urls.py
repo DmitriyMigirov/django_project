0ecdb89b7f420a76b1211ee8c35a18ad479929fb
DmytroMigirov <dmitriystels@gmail.com>
2022-09-28 19:52:15 +0300

    Created app for items with basic structure items.urls

--- onlinestore/urls.py
+++ onlinestore/urls.py
@@ -19,3 +19,18 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.urls import include
+from django.urls import path
+urlpatterns += [
+    path('items/', include('items.urls')),
+]
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/items/', permanent=True))
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
